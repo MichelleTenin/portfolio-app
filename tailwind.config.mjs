d8eae9bf693c4f1e3d0ec895c082bd7e90b6f1f8
Michelle Tenin <60997932+MichelleTenin@users.noreply.github.com>
2025-10-13 21:59:52 -0700

    initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./src/**/*.{astro,html,js,jsx,ts,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [
+      require('daisyui'), // add this line
+    ],
+    daisyui: {
+        themes: ["light", "dark", "cupcake"], // optional theme selection
+      },
+  }
+  
